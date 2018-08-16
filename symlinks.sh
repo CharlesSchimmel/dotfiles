506c721feaa7e3c5c60654d53c6b7ab2312b1250
Charles Schimmelpfennig <charles.schimmelpfennig@pioneer.com>
2018-08-16 10:58:19 -0500

    add ftplugins symlink

--- symlinks.sh
+++ symlinks.sh
@@ -3,6 +3,6 @@
 ln -s $HOME/dots/aliases $HOME/.aliases
 ln -s $HOME/dots/inputrc $HOME/.inputrc
 ln -s $HOME/dots/vimrc $HOME/.vimrc
-ln -s $HOME/dots/vim $HOME/.vim
+ln -s $HOME/dots/vim/ftplugin $HOME/.vim/ftplugin
 ln -s $HOME/dots/zshrc $HOME/.zshrc
 ln -s $HOME/dots/tmux.conf $HOME/.tmux.conf
