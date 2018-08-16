193c2206538e9197e8b87e6b490351ab37831ebf
CharlesSchimmel <charlesschimmel@gmail.com>
2018-08-15 20:30:45 -0500

    redundant .vim

--- symlinks.sh
+++ symlinks.sh
@@ -3,6 +3,7 @@
 ln -s $HOME/dots/aliases $HOME/.aliases
 ln -s $HOME/dots/inputrc $HOME/.inputrc
 ln -s $HOME/dots/vimrc $HOME/.vimrc
+ln -s $HOME/dots/vim $HOME/.vim
 ln -s $HOME/dots/xinitrc $HOME/.xinitrc
 ln -s $HOME/dots/Xresources $HOME/.Xresources
 ln -s $HOME/dots/Xresources.d $HOME/.Xresources.d
