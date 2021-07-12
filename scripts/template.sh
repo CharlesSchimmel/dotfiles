d5709ca69e67cd8e03deb129aa47b2343f14f534
Charles Schimmelpfennig <charles.schimmelpfennig@corteva.com>
2021-07-12 18:12:55 -0500

    add updated template script

---
+++ scripts/template.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# to be used with:
+# au BufNewFile $vimwikidir/*.md :silent 0r !$vimwikidir/template.sh '%'
+
+filename="$(basename "$@" '.md')"
+# don't forget that the 3-character date prefix IS case-sensitive...
+prefix="$(printf "%s" "$filename" | sed 's/^\([a-z][0-9a-c][0-9a-z]\).\+/\1/g')"
+rest_of_title="$(printf "%s" "$filename" | sed 's/^[a-z][0-9a-c][0-9a-z]\(.\+\)/\1/g')"
+title_cased="$(echo $rest_of_title | sed -e 's/.*/\L&/; s/[a-z]*/\u&/g' -e 's/^[l-z]../\U&/g')"
+title="$prefix $title_cased"
+
+cat << EOF
+---
+title: $title
+date: $(date +%Y-%m-%d)
+updated: $(date +%Y-%m-%d)
+---
+# $title
+EOF
